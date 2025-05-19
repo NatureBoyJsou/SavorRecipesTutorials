1523c98d6794785ad310fd125eb4244520d77ee5
NatureBoyJsou <100043411+NatureBoyJsou@users.noreply.github.com>
2025-05-19 13:47:03 -0400

    Update Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import CookingTutorials from './CookingTutorials';
 
-
-
 function RouterApp() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cooking-tutorials" element={<CookingTutorials />} />
         {/* Add other routes here as needed */}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
